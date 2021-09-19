a2fd19a08357dafcfa73d7ef1c8506b83a5a7535
Khoa Tran <khoadtran@Khoas-iMac.local>
2021-09-19 09:04:26 +0700

    Init code

---
+++ proxy.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+
+const express = require('express');
+const morgan = require("morgan");
+const {createProxyMiddleware} = require('http-proxy-middleware');
+const cors = require('cors');
+
+// Create Express Server
+const app = express();
+
+// CORS Configuration
+app.use(cors());
+
+// Configuration
+const PORT = 1402;
+const API_SERVICE_URL = "https://pro-api.coinmarketcap.com";
+
+// Logging
+app.use(morgan('dev'));
+
+// Proxy endpoints
+app.use('/v1/cryptocurrency/listings/latest', createProxyMiddleware({
+	target: API_SERVICE_URL,
+	headers: {
+		'X-CMC_PRO_API_KEY': process.env.COINCAP_API_KEY
+	},
+	changeOrigin: true,
+}));
+
+// Start Proxy
+app.listen(PORT, () => {
+	console.log(`Starting Proxy on port ${PORT}`);
+});
